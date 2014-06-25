b94a627fe3876526ae4608481bac0a679904bb0d
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-06-25 22:58:03 +0200

    Add Libs for library namespace

---
+++ src/js/content/libs.js
@@ -0,0 +1,23 @@
+/*global Utils, I18n*/
+// This creates a "safe" namespace for all libs
+var Libs = {
+  _libs: {},
+  add: function(libraryName, librayTopLevelFunction) {
+    // Check if the method is already defined
+    if(this._libs[libraryName] || this.hasOwnProperty(libraryName)) {
+      Utils.log("[libs.js] Can not add library named '" + libraryName + "' because it already exists as a function().");
+      return;
+    }
+    this[libraryName] = librayTopLevelFunction;
+    Utils.log("[libs.js] Added library as Libs." + libraryName);
+  }
+};
+
+// Add vendored chance.js
+Libs.add("chance", new window.Chance());
+window.chance = null;
+
+// Add vendored moment.js and set locale
+Libs.add("moment", window.moment);
+Libs.moment.lang(I18n.userLocale());
+window.moment = null;
