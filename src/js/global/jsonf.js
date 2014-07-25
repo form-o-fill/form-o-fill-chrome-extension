72bd64cee31eeda718195635d84befbdb78aef52
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-07-25 21:57:22 +0200

    JSONF can serialize/deserialize RegExp objects. fixes #17

--- src/js/global/jsonf.js
+++ src/js/global/jsonf.js
@@ -2,31 +2,40 @@
 "use strict";
 var JSONF = {
   stringify: function(object) {
-    return JSON.stringify(object, this._functionSerializer);
+    return JSON.stringify(object, this._serializer);
   },
   parse: function(jsonString) {
-    return JSON.parse(jsonString, this._functionDeserializer);
+    return JSON.parse(jsonString, this._deserializer);
   },
-  _functionSerializer: function(key, value) {
-    if (typeof (value) === "function") {
+  _serializer: function(key, value) {
+    // Is a FUNCTION or REGEXP ?
+    if (typeof (value) === "function" || typeof value.test === "function") {
       return value.toString();
     }
     return value;
   },
-  _functionDeserializer: function(key, value) {
-    if (key === "" && typeof value === "string" && value.indexOf("function") !== 0) {
+  _deserializer: function(key, value) {
+    if (key === "" && typeof value === "string" && value.indexOf("function") !== 0 && value.indexOf("/") !== 0) {
       return value;
     }
 
     if (typeof value === "string") {
       var rfunc = /^function\s*\((.*?)\).*?\{([\s\S]*)\}/m;
+      var rregexp = /^\/(.*?)\/$/m;
       var match = value.match(rfunc);
 
+      // Function?
       if (match) {
         var args = match[1].split(',').map(function(arg) {
           return arg.replace(/\s+/, '');
         });
         return new Function(args, match[2].trim());
+      }
+
+      // RegEx?
+      match = value.match(rregexp);
+      if (match) {
+        return new RegExp(match[1]);
       }
     }
     return value;
