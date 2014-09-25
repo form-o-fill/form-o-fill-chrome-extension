01db1968ca160dd8e47d745cbfae3081b1f5939e
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-09-25 10:58:48 +0200

    move a single rule into rule.js

---
+++ src/js/global/rule.js
@@ -0,0 +1,56 @@
+/*global Logger, jQuery */
+
+/* A single Rule */
+var Rule = function() {
+  this.prettyPrint = function() {
+    var clone = jQuery.extend({}, this);
+    delete clone.matcher;
+    delete clone.nameClean;
+    delete clone.urlClean;
+    delete clone.id;
+    delete clone._escapeForRegexp;
+    return JSON.stringify(clone, null, 2);
+  };
+
+  this.prettyPrintHtml = function() {
+    return this.prettyPrint().replace(/\n/,"<br />");
+  };
+
+  this._escapeForRegexp = function(str) {
+    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+  };
+};
+
+Rule.create = function(options, tabId, ruleIndex) {
+  delete options.matcher;
+  delete options.nameClean;
+  delete options.urlClean;
+  delete options._escapeForRegexp;
+  delete options.prettyPrint;
+  delete options.prettyPrintHtml;
+  var rule = new Rule();
+  Object.keys(options).forEach(function(key) {
+    rule[key] = options[key];
+  });
+
+  // RegExp in URL or string?
+  if(typeof rule.url !== "undefined" && typeof rule.url.test !== "undefined") {
+    // RegExp
+    rule.matcher = new RegExp(rule.url);
+  } else if(typeof rule.url !== "undefined") {
+    // String (match full url only)
+    rule.matcher = new RegExp("^" + rule._escapeForRegexp(rule.url) + "$");
+  }
+
+  if (typeof rule.url !== "undefined") {
+    rule.urlClean = rule.url.toString();
+  } else {
+    rule.urlClean = "n/a";
+  }
+  rule.nameClean = rule.name.replace("<", "&lt;");
+  if(typeof rule.id === "undefined") {
+    rule.id = tabId + "-" + ruleIndex;
+  }
+  Logger.info("[rule.js] created rule", rule);
+  return rule;
+};
