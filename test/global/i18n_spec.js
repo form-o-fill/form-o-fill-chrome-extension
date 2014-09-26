a54947a973e0a557cf611713ef0f336f9948b59b
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-09-26 11:34:09 +0200

    more specs

--- test/global/i18n_spec.js
+++ test/global/i18n_spec.js
@@ -1,11 +1,24 @@
 var I18n = require("../../src/js/global/i18n.js");
-global.chrome = require("../support/chrome_api.js");
 
 describe("I18n", function(){
+
   describe(".supportedLanguages", function() {
     it("includes english in the supported languages", function(){
-      expect(I18n.supportedLanguages).to.include("en");
+      expect(I18n.supportedLanguages()).to.include("en");
     });
+  });
+
+  describe(".current_locale", function() {
+    it("returns the default locale if the user locale isn't covered", sinon.test(function(){
+      global.chrome.setConfig("uiLanguage", "xy_zz");
+      expect(I18n.currentLocale()).to.eql("en");
+    }));
+
+    it("returns the default locale if the user locale isn't covered", sinon.test(function(){
+      this.stub(I18n, "supportedLanguages").returns(["en", "de"]);
+      global.chrome.setConfig("uiLanguage", "de_DE");
+      expect(I18n.currentLocale()).to.eql("de");
+    }));
   });
 
   describe(".userLocale", function() {
@@ -18,6 +31,12 @@ describe("I18n", function(){
       global.chrome.setConfig("uiLanguage", "EN_GB");
       expect(I18n.userLocale()).to.eq("en");
     });
+  });
+
+  xdescribe(".load_pages", function() {
+    it("loads localized pages", sinon.test(function(){
+      I18n.loadPages(["about"]);
+    }));
   });
 });
 
