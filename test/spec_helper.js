6acc57a2bdd26e22982991ad73b047b27536a56f
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-09-26 21:02:51 +0200

    add stub for Logger

--- test/spec_helper.js
+++ test/spec_helper.js
@@ -1,6 +1,3 @@
-var path = require("path");
-var fs = require("fs");
-
 // Expectation Framework
 global.chai = require("chai");
 global.chai.use(require("sinon-chai"));
@@ -18,14 +15,6 @@ global.chrome = require("./support/chrome_api.js");
 // jQuery loaded with the jsDOM window
 global.jQuery = require("./support/jquery.js")(jsdom.jsdom().parentWindow);
 
-// Absolute path to test directory
-global.testRoot = path.normalize(__dirname);
-
-// Load the source into a string so they
-// can be used with jsdom as "src" option
-var sources = {
-  jQuery: fs.readFileSync(testRoot + '/support/jquery.js').toString(),
-  i18n: fs.readFileSync(testRoot + '/../src/js/global/i18n.js').toString(),
-  chrome: fs.readFileSync(testRoot + '/support/chrome_api.js').toString()
+global.Logger = {
+  info: function() {}
 };
-global.sources = sources;
