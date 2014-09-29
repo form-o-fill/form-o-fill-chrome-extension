cf948b334c19e049f709140e493dbc03ddc2f331
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-09-29 21:52:01 +0200

    how to test promises
    - install chai-as-promised
    - return an expect().to.eventually from the it() block
      as provided by chai-as-promised

--- test/global/rules_spec.js
+++ test/global/rules_spec.js
@@ -2,6 +2,16 @@ var Rules = require("../../src/js/global/rules.js");
 
 describe("Rules", function() {
   describe(".match", function() {
+
+    beforeEach(function() {
+      sinon.stub(Rules, "all").returns(new Promise(function(resolve) {
+        resolve([{url: /test\.html/}, {url: /test2\.html/}]);
+      }));
+    });
+
+    it("returns all rules with matching url", function(){
+      return expect(Rules.match("test2.html")).to.become([ { url: /test2\.html/ } ]);
+    });
   });
 
   describe(".load", function() {
