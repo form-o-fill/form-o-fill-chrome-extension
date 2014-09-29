5df49e3e99a942ae3e481343092153798f515128
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-09-29 22:11:13 +0200

    finish Rules.all test

--- test/global/rules_spec.js
+++ test/global/rules_spec.js
@@ -3,14 +3,29 @@ var Rules = require("../../src/js/global/rules.js");
 describe("Rules", function() {
   describe(".match", function() {
 
-    beforeEach(function() {
+    var stubRules = function(out) {
       sinon.stub(Rules, "all").returns(new Promise(function(resolve) {
-        resolve([{url: /test\.html/}, {url: /test2\.html/}]);
+        resolve(out);
       }));
+    };
+
+    afterEach(function () {
+      Rules.all.restore();
     });
 
     it("returns all rules with matching url", function(){
+      stubRules([{url: /test\.html/}, {url: /test2\.html/}]);
       return expect(Rules.match("test2.html")).to.become([ { url: /test2\.html/ } ]);
+    });
+
+    it("returns an empty array if no rules match", function(){
+      stubRules([{url: /test\.html/}, {url: /test2\.html/}]);
+      return expect(Rules.match("nomatch.html")).to.eventually.eql([]);
+    });
+
+    it("ignores rules which have no url (broken)", function(){
+      stubRules([{nourl: /test\.html/}, {url: /test2\.html/}]);
+      return expect(Rules.match("test.html")).to.eventually.eql([]);
     });
   });
 
