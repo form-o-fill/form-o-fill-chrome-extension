1562b1c00a7669ba4c902a15f9f8ed626d72d25c
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-10-20 11:30:11 +0200

    unify the way to output testing infos

--- src/js/content/testing.js
+++ src/js/content/testing.js
@@ -2,7 +2,19 @@
 // This file is for end to end testing only
 // It is delivered with the production code but disabled
 var installTestingCode = function() {
-  var $info = jQuery("#form-o-fill-testing-info");
+
+  var Testing = {
+    setTestingVar: function(key, value, text) {
+      var $info = jQuery("#form-o-fill-testing-info");
+      var foundEl = $info.find("." + key);
+      // When the text property is set, append that to the DOM
+      if(foundEl.length === 0 && typeof text !== "undefined") {
+        $info.append("<tr><td>" + text + "</td><td class='" + key + "'>" + value + "</td></tr>");
+      } else {
+        $info.find("." + key).html(value);
+      }
+    }
+  };
 
   // Tell the background page that we are in testing mode
   chrome.runtime.sendMessage({action: "setTestingMode", value: true}, function(bgInfo) {
@@ -11,24 +23,17 @@ var installTestingCode = function() {
     // That information is then picked up by the integration tests to reach intern URLs like
     // the options page
     Logger.info("[c/testing.j] background.js has set testing mode to " + bgInfo.testingMode);
-    $info.find(".extension-id").html(bgInfo.extensionId).end()
-    .find(".tab-id").html(bgInfo.tabId).end()
-    .find(".extension-version").html(bgInfo.extensionVersion).end()
-    .find(".testing-mode").html(bgInfo.testingMode);
+    Testing.setTestingVar("extension-id", bgInfo.extensionId, "Extension Id");
+    Testing.setTestingVar("tab-id", bgInfo.tabId, "TabId of this page");
+    Testing.setTestingVar("extension-version", bgInfo.extensionVersion, "Form-O-Fill Version");
+    Testing.setTestingVar("testing-mode", bgInfo.testingMode, "Testing mode");
   });
 
   // Listen to messages from background.js
   chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-
     // Set a variable in the DOM based on what is sent from bg.js
     if(message.action === "setTestingVar" && message.key && typeof message.value !== "undefined") {
-      var foundEl = $info.find("." + message.key);
-      // When the text property is set, append that to the DOM
-      if(foundEl.length === 0 && typeof message.text !== "undefined") {
-        $info.append("<tr><td>" + message.text + "</td><td class='" + message.key + "'>" + message.value + "</td></tr>");
-      } else {
-        $info.find("." + message.key).html(message.value);
-      }
+      Testing.setTestingVar(message.key, message.value, message.text);
       sendResponse(true);
     }
   });
