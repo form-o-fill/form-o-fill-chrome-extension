206a4eecdf7a11d3b7b8ecf9f7d55351bcb7c587
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-11-06 20:41:35 +0100

    check more fields

--- test/integration/options_scene.js
+++ test/integration/options_scene.js
@@ -1,27 +1,51 @@
 describe("the options panel", function() {
 
+  var visitOptions = function() {
+    return Tests.visit("options")
+    .then(function () {
+      return Tests.importRules();
+    })
+    .then(function () {
+      return $(".extension-options-url a").click();
+    }).then(function () {
+      return browser.driver.sleep(500);
+    });
+  };
+
   it("is available", function(){
-    //Tests.visit("options")
-    browser.driver.get("http://localhost:8889/form-o-fill-testing/options.html")
-    .then(function () {
-      console.log("IR");
-      Tests.importRules();
-    })
-    .then(function () {
-      return $(".extension-options-url a").getAttribute("href");
-    })
-    .then(function (optionsUrl) {
-      return browser.driver.executeScript("window.location.href='" + optionsUrl + "';");
-    })
-    .then(function () {
-      return browser.driver.getCurrentUrl();
-    })
-    .then(function (url) {
-      // open options in the same window!
-      // Doesn't work at the moment because the options open in a new tab
-      console.log(url);
-      //expect($("li.tab.current input").getAttribute("value")).to.become("dummy");
+    visitOptions().then(function () {
+      expect(browser.driver.getTitle()).to.become("Form-O-Fill Options");
     });
   });
+
+  it("is contains all essential parts", function(){
+    visitOptions().then(function () {
+      // Current tab
+      expect($("li.tab.current input").getAttribute("value")).to.become("dummy");
+
+      // More tab
+      expect($("li.tab.more").isPresent()).to.become(true);
+
+      // Quickump menu
+      expect($("#rules-overview").isPresent()).to.become(true);
+
+      // Action buttons
+      expect($(".menu button.save").isPresent()).to.become(true);
+      expect($(".menu button.reload").isPresent()).to.become(true);
+      expect($(".menu button.format").isPresent()).to.become(true);
+      expect($(".menu button.export").isPresent()).to.become(true);
+      expect($(".menu button.import").isPresent()).to.become(true);
+
+      // Editor window
+      $("#ruleeditor-ace").getSize().then(function (size) {
+        expect(size.height).to.be.above(200);
+        expect(size.width).to.be.above(200);
+      });
+
+      // Navigation links
+      expect($$(".navigation .menu li").getText()).to.become(["Rule Editor", "Help", "About", "Changelog", "Logs"]);
+    });
+  });
+
 
 });
