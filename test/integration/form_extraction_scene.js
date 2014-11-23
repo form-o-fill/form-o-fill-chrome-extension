e7b1445725c497f591fa246305bf85232c8ae57b
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-11-23 21:25:25 +0100

    uncomment spec

--- test/integration/form_extraction_scene.js
+++ test/integration/form_extraction_scene.js
@@ -1,6 +1,6 @@
 describe("the form extraction", function() {
 
-  xit("shows the extraction overlay", function(){
+  it("shows the extraction overlay", function(){
     Tests.visit("form-extraction")
     .then(element(by.cssContainingText(".popup-html a.cmd-show-extract-overlay", "Extract")).click)
     .then(function () {
@@ -9,6 +9,8 @@ describe("the form extraction", function() {
   });
 
   it("shows a notification when the user clicks the overlay", function(){
+    // I can't explain why this and ONLY this form (mixed then(func) and then(return func))
+    // gives a usable result. Bugs me.
     Tests.visit("form-extraction")
     .then(element(by.cssContainingText(".popup-html a.cmd-show-extract-overlay", "Extract")).click)
     .then($(".form-o-fill-overlay-form").click)
