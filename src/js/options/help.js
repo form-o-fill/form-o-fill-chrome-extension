0e5092890cbe8a0fe19f479a3619f1e7a1643e88
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-12-07 21:09:40 +0100

    add help for context.findHtml()

--- src/js/options/help.js
+++ src/js/options/help.js
@@ -4,6 +4,6 @@
 // See also i18n.js
 jQuery(document).on("i18n-loaded", function (event, pageName) {
   if (pageName.indexOf("help_") > -1) {
-    I18n.loadPages(["help-capture", "help-filling", "help-editor", "help-basic", "help-functions", "help-libraries", "help-ownlibraries", "help-before", "help-sharedrules"], "help-content");
+    I18n.loadPages(["help-capture", "help-filling", "help-editor", "help-basic", "help-functions", "help-libraries", "help-ownlibraries", "help-before", "help-beforecontext", "help-sharedrules"], "help-content");
   }
 });
