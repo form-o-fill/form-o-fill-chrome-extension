c56b6f4cbc2643952d9c8603c985683bb1975496
Frank Schumacher <frank.schumacher.privat@gmail.com>
2015-01-06 22:12:41 +0100

    workflow related JS

--- src/js/options/workflow.js
+++ src/js/options/workflow.js
@@ -1,8 +1,49 @@
-/* global Workflows jQuery*/
-jQuery(function () {
+/* global Workflows jQuery Rules*/
+var workflows = [];
 
+var emptyWorkflow = {
+  name: "new workflow",
+  steps: []
+};
+
+var stepHtml = function(text) {
+  return '<li>' + text + '<button class="wf-delete-step">remove step</button></li>';
+};
+
+var ruleHtml = function(rule) {
+  return '<option data-rule-name="' + rule.name + '" data-rule-id="' + rule.id + '">' + rule.nameClean + '</option>';
+};
+
+// Create a new workflow
+var createWorkflow = function () {
+  jQuery(".wf-name").val(emptyWorkflow.name);
+  jQuery("#workfloweditor ol li").remove();
+  jQuery("#workfloweditor").show();
+  jQuery(".wf-name").trigger("focus").select();
+};
+
+var fillRuleSelect = function(rules) {
+  jQuery(".rulelist").html(rules.map(ruleHtml).join(""));
+};
+
+var addStepToWorkflow = function () {
+  var ruleName = jQuery(".rulelist option:selected").data("ruleName");
+  if(typeof ruleName !== "undefined") {
+    jQuery("#workfloweditor ol").append(stepHtml(ruleName));
+  }
+};
+
+/* load available rules and fill select field */
+Rules.all().then(function availableRules(rules) {
+  jQuery(".rulelist option").remove();
+  fillRuleSelect(rules);
+});
+
+jQuery(function () {
   /* Load all present workflows and fill select field */
   Workflows.load().then(function loadWf(rawWorkflows) {
+    workflows = rawWorkflows;
+
     var $wfSelect = jQuery(".wf-all select");
     var optionHtml = [];
 
@@ -13,7 +54,17 @@ jQuery(function () {
         return "<option>" + wfData.name + "</option>";
       }));
     }
-
     $wfSelect.html(optionHtml.join());
+  });
+
+  /* 'create workflow' button */
+  jQuery(".wf-add-wf").on("click", createWorkflow);
+
+  /* 'add to workflow' button */
+  jQuery(".wf-add-step").on("click", addStepToWorkflow);
+
+  /* 'remove step' buttons */
+  jQuery(document).on("click", ".wf-delete-step", function wfDeleteStep() {
+    jQuery(this).parent().remove();
   });
 });
