1e7f0ef5418a5305513b1927f30d38186da301ad
Frank Schumacher <frank.schumacher.privat@gmail.com>
2015-01-20 20:39:02 +0100

    better function parsing when deserializing

--- src/js/global/jsonf.js
+++ src/js/global/jsonf.js
@@ -25,16 +25,16 @@ var JSONF = {
     }
 
     if (typeof value === "string") {
-      var rfunc = /^function\s*\((.*?)\)[\s\S]*?\{([\s\S]*)\}/m;
+      var rfunc = /^function\s*(\w*)\s*\((.*?)\)[\s\S]*?\{([\s\S]*)\}/m;
       var rregexp = /^\/(.*?)\/$/m;
       var match = value.match(rfunc);
 
       // Function?
       if (match) {
-        var args = match[1].split(',').map(function(arg) {
+        var args = match[2].split(',').map(function(arg) {
           return arg.replace(/\s+/, '');
         });
-        return new Function(args, match[2].trim());
+        return new Function(args, match[3].trim());
       }
 
       // RegEx?
