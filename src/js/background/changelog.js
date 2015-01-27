60a7ad6a6e31b191ce0d57291e746aa3baf62e14
Frank Schumacher <frank.schumacher.privat@gmail.com>
2015-01-27 21:40:02 +0100

    changelog notification 2.0

--- src/js/background/changelog.js
+++ src/js/background/changelog.js
@@ -1,8 +1,8 @@
 /*eslint no-unused-vars: 0 */
 var Changelog = {
   changes: {
-    "1.6.1": {
-      title: "Workflows have arrived!",
+    "2.0.0": {
+      title: "2.0.0 : Workflows have arrived!",
       message: "Chain rules together to form sequences of actions. Even survives page reload!",
       target: "#workflows"
     }
