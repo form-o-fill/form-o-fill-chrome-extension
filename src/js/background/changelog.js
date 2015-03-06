988ef5a6a3597f1353e667f71710fb4504bc51ba
Frank Schumacher <frank.schumacher.privat@gmail.com>
2015-03-06 20:48:37 +0100

    changelog notification for 2.2.1

--- src/js/background/changelog.js
+++ src/js/background/changelog.js
@@ -5,6 +5,11 @@ var Changelog = {
       title: "2.0.0 : Workflows have arrived!",
       message: "Chain rules together to form sequences of actions. Even survives page reload!",
       target: "#workflows"
+    },
+    "2.2.1": {
+      title: "FoF updated: Save data between rules!",
+      message: "Save data between rule executions. Ideal for workflows.",
+      target: "#help-before-context"
     }
   },
   findForVersion: function(version) {
