121f1f6823fb14710d5bf5bd7475ee1c18b4e48a
Frank Schumacher <frank.schumacher.privat@gmail.com>
2015-07-13 21:13:01 +0200

    update moment.js to 2.10.3

--- src/js/global/utils.js
+++ src/js/global/utils.js
@@ -22,7 +22,7 @@ var Utils = {
   reservedLibNamespaces: ["h", "halt"],
   vendoredLibs: {
     "vendor/chance.js/chance.js": { detectWith: /Libs\.chance/, name: "chance", onWindowName: "chance" },
-    "vendor/moment.js/moment-with-langs.js": { detectWith: /Libs\.moment/, name: "moment", onWindowName: "moment" }
+    "vendor/moment.js/moment-with-locales.min.js": { detectWith: /Libs\.moment/, name: "moment", onWindowName: "moment" }
   },
   isLiveExtension: function() {
     return window.location.host === Utils.liveExtensionId;
