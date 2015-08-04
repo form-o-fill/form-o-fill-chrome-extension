1f6d610049dac9c1b73ad05da39b878d4e694b00
Frank Schumacher <frank.schumacher.privat@gmail.com>
2015-08-04 22:37:08 +0200

    allow setting of storage inside value functions
    the scope is ALL value functions

--- src/js/content/context.js
+++ src/js/content/context.js
@@ -1,7 +1,8 @@
 /*global JSONF */
 /*eslint no-unused-vars: 0 */
 // This is not the same context as in background.js
-// Currently it only allows to read storage values set by bg.js
+// Currently it only allows to read storage values set by bg.js but
+// you can set value for all value functions to access
 var context = {
   storage: {
     get: function(key) {
@@ -10,6 +11,10 @@ var context = {
         return JSONF.parse(value);
       }
       return value;
+    },
+    set: function(key, value) {
+      // set it in localstorage
+      window.sessionStorage.setItem(key, JSONF.stringify(value));
     }
   }
 };
