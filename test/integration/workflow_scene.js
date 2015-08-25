c8bf325f368f57cc3dd74be3ad27e134c409afa0
Frank Schumacher <frank.schumacher.privat@gmail.com>
2015-08-25 18:47:02 +0200

    add workflows scene

---
+++ test/integration/workflow_scene.js
@@ -0,0 +1,15 @@
+require("../support/integration_helper");
+
+describe("using workflows", function() {
+
+  it("works", function (done) {
+    Tests.visit("workflow-1")
+    .click(".popup-html .select-workflow")
+    .pause(global.pause)
+    .url().then(function(currentUrl) {
+      expect(currentUrl.value).to.eql("http://localhost:9292/form-o-fill-testing/workflow-3.html?");
+    })
+    .call(done);
+  });
+});
+
