c4f5b390710693fd98197bc95098f301f3955793
Frank Schumacher <frank.schumacher.privat@gmail.com>
2015-12-05 21:16:07 +0100

    add remote URL testcase

---
+++ testcases/docroot-for-testing/form-o-fill-testing/fof-complete-complex-2.js
@@ -0,0 +1,17 @@
+{
+  "workflows": [],
+  "rules": {
+    "tabSettings": [
+      {
+        "id": "1",
+        "name": "remote import URL"
+      }
+    ],
+    "rules": [
+      {
+        "code": "var rules = [{\n  name: \"Complex - 2 - remote URL\",\n  url: /22-complex-2\\.html/,\n  fields: [{\n    selector: \"#target11\",\n    value: \"remote import!\"\n  }]\n}\n];",
+        "tabId": 1
+      }
+    ]
+  }
+}
