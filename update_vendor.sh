91607abb68dd2f31ad4c0ce37b9cf351455e8e91
thenoseman <frank.schumacher.privat@gmail.com>
2017-11-14 21:02:48 +0100

    update math.js to 3.16.5

--- update_vendor.sh
+++ update_vendor.sh
@@ -6,3 +6,4 @@
 curl -o src/vendor/moment.js/moment-with-locales.min.js -LO "https://momentjs.com/downloads/moment-with-locales.min.js" 
 curl -o src/vendor/chance.js/chance.js -LO "https://raw.githubusercontent.com/chancejs/chancejs/master/dist/chance.min.js"
 curl -o src/vendor/optimal-select/optimal-select.js -LO "https://raw.githubusercontent.com/Autarc/optimal-select/master/dist/optimal-select.min.js"
+curl -o src/vendor/math.js/math.min.js -LO "https://raw.githubusercontent.com/josdejong/mathjs/master/dist/math.min.js"
