717b9c0d129ef3e89fbb2e98e60467116dc40e5f
thenoseman <frank.schumacher.privat@gmail.com>
2017-11-14 20:07:28 +0100

    update chance.js and moment.js

---
+++ update_vendor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Updates:
+# moment.js
+# chance.js
+#
+curl -o src/vendor/moment.js/moment-with-locales.min.js -LO "https://momentjs.com/downloads/moment-with-locales.min.js" 
+curl -o src/vendor/chance.js/chance.js -LO "https://raw.githubusercontent.com/chancejs/chancejs/master/dist/chance.min.js"
